ad51ca096f9129eb063224043955d22cc6c4af9a
aayu12345 <147895498+aayu12345@users.noreply.github.com>
2024-12-25 13:11:16 +0530

    Move all zeroes to the end

---
+++ Problem 1.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.util.Arrays;
+
+class Main {
+
+    // Function which pushes all zeros to end of array
+    static void pushZerosToEnd(int[] arr) {
+
+        // Pointer to track the position for next non-zero element
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // If the current element is non-zero
+            if (arr[i] != 0) {
+
+                // Swap the current element with the 0 at index 'count'
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                // Move 'count' pointer to the next position
+                count++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
